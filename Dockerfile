49cd0a200be525f5fd94c261ee351840618646ec
Mihaela Trajkovska <mihaelatrajkovska0gmail.com>
2025-12-09 16:03:32 +0100

    Final project version – CI/CD, Docker, Kubernetes configuration completed

--- Dockerfile
+++ Dockerfile
@@ -5,20 +5,20 @@ WORKDIR /app
 # netcat за wait-for-it.sh
 RUN apt-get update && apt-get install -y netcat-openbsd && apt-get clean
 
-# dependencies
+
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
-# код
+
 COPY . .
 
-# FLASK променливи
+
 ENV FLASK_APP=app.py
 ENV FLASK_RUN_HOST=0.0.0.0
 ENV FLASK_RUN_PORT=5000
 
-# дозволи скрипта
+
 RUN chmod +x wait-for-it.sh
 
-# стартувај после што ќе проработи Postgres
+
 CMD ["./wait-for-it.sh", "db", "5432", "--", "flask", "run"]
--- backend/config.py
+++ backend/config.py
@@ -2,13 +2,26 @@
 # import os
 #
 # class Config:
-#     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "postgresql://postgres:postgres@db:5432/massage_db")
+#     SQLALCHEMY_DATABASE_URI = os.environ.get(
+#         "DATABASE_URL",              # Docker / Kubernetes
+#         "sqlite:///instance/massage.db"  # локално без Docker
+#     )
 #     SQLALCHEMY_TRACK_MODIFICATIONS = False
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DATABASE_URL",              # Docker / Kubernetes
-        "sqlite:///instance/massage.db"  # локално без Docker
-    )
+
+    database_url = os.environ.get("DATABASE_URL")
+
+    if not database_url:
+        db_user = os.environ.get("DB_USER")
+        db_password = os.environ.get("DB_PASSWORD")
+        db_host = os.environ.get("DB_HOST")
+        db_name = os.environ.get("DB_NAME")
+        if db_user and db_password and db_host and db_name:
+            database_url = f"postgresql://{db_user}:{db_password}@{db_host}:5432/{db_name}"
+        else:
+            database_url = "sqlite:///instance/massage.db"
+
+    SQLALCHEMY_DATABASE_URI = database_url
     SQLALCHEMY_TRACK_MODIFICATIONS = False
